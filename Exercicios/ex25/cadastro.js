eb89584ea52c246ad6d133c46b412148f2363947
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-03-08 10:49:53 -0300

    Organizando exercicios

--- Exercicios/ex25/cadastro.js
+++
@@ -1,6 +0,0 @@
-function clicou(){
-let nome = document.querySelector('input#nome')
-let sobrenome = document.querySelector('input#sobrenome')
-let res = document.querySelector('div#res')
-res.innerHTML = `Olá, ${nome} ${sobrenome}, seja bem vindo!`
-}
